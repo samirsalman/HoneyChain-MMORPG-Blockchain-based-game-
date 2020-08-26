df68160e08d268d9ef6c85db130851f2c43d8c70
Samir <samirsalman1997@gmail.com>
2020-08-26 11:53:14 +0200

    Update response.js

--- server/api/response.js
+++ server/api/response.js
@@ -3,21 +3,37 @@ const router = express.Router();
 
 router.get("/login/success", async (req, res, next) => {
   res.statusCode = 200;
+  if (req.headers.cookie !== undefined) {
+    res.setHeader("Cookie", req.headers.cookie);
+  }
+  console.log(res.getHeaders());
   res.sendStatus(200);
 });
 
 router.get("/login/error", async (req, res, next) => {
   res.statusCode = 500;
+  if (req.headers.cookie !== undefined) {
+    res.setHeader("Cookie", req.headers.cookie);
+  }
+  console.log(res.getHeaders());
   res.sendStatus(500);
 });
 
 router.get("/registration/success", async (req, res, next) => {
   res.statusCode = 200;
+  if (req.headers.cookie !== undefined) {
+    res.setHeader("Cookie", req.headers.cookie);
+  }
+  console.log(res.getHeaders());
   res.sendStatus(200);
 });
 
 router.get("/registration/error", async (req, res, next) => {
   res.statusCode = 500;
+  if (req.headers.cookie !== undefined) {
+    res.setHeader("Cookie", req.headers.cookie);
+  }
+  console.log(res.getHeaders());
   res.sendStatus(500);
 });
 
