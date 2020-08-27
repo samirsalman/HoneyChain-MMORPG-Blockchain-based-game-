d5b50aabdf236bd44d03c289f730bbd777d6f1af
Samir <samirsalman1997@gmail.com>
2020-08-27 16:44:13 +0200

    api

--- server/api/user/register/registerUser.js
+++ server/api/user/register/registerUser.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const database = require("../../database/database");
-const User = require("../../../models/user");
-var hash = require("object-hash");
 //endpoint to register user
-router.post("/", async (req, res, next) => {
-  /*Verify name and email not exists
-  if true create and save to mongoDB the user and initialize player
-else return error
 
-*/
-  var user = new User(
-    req.body.email,
-    hash(req.body.password),
-    req.body.name,
-    req.body.years
-  );
-  await database.createUser(user).catch((err) => {
-    console.error(err);
+const axios = require("axios");
+
+const HOST = "http://localhost:3001/registerUser";
+
+router.post("/", async (req, res, next) => {
+  axios.default.get(`${HOST}?username=${req.query.email}`).then((res) => {
+    res.sendStatus(200);
   });
-  res.send(user);
 });
 
 module.exports = router;
