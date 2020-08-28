7d9900a917450a437657bd9c850b8ceb55b88599
Samir <samirsalman1997@gmail.com>
2020-08-28 12:35:46 +0200

    Update response.js

--- server/api/response.js
+++ server/api/response.js
@@ -25,19 +25,21 @@ router.get("/login/error", async (req, res, next) => {
   res.sendStatus(500);
 });
 
-router.get("/registration/success", async (req, res, next) => {
+router.get("/registration/success", (req, res, next) => {
   res.statusCode = 200;
   if (req.headers.cookie !== undefined) {
     res.setHeader("Cookie", req.headers.cookie);
   }
   console.log(res.getHeaders());
-
-  axios.default.get(`${HOST}?username=${req.query.email}`).then(() => {
-    res.sendStatus(200);
-  });
+  axios.default
+    .get(`${HOST}?username=${req.query.email}`)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => res.send(err));
 });
 
-router.get("/registration/error", async (req, res, next) => {
+router.get("/registration/error", (req, res, next) => {
   res.statusCode = 500;
   if (req.headers.cookie !== undefined) {
     res.setHeader("Cookie", req.headers.cookie);
