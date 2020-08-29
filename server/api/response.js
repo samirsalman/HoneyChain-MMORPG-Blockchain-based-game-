99873516d9600a38d5b6d5571bd45b6466f7ee1e
Samir <samirsalman1997@gmail.com>
2020-08-29 17:33:59 +0200

    Update response.js

--- server/api/response.js
+++ server/api/response.js
@@ -22,7 +22,7 @@ router.get("/login/success", async (req, res, next) => {
   connection.query(
     `SELECT * FROM report_login WHERE cookie="${
       req.headers.cookie.split("login=")[1]
-    }"`,
+    }"; expires`,
     function (error, emailResult, fields) {
       if (error) throw error;
       console.log(emailResult[0].email);
