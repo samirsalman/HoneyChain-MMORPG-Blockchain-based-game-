0a1abda93e1690a6198f6307e901e455e298f58b
Samir <samirsalman1997@gmail.com>
2020-08-29 12:18:53 +0200

    mysql impl

--- server/api/response.js
+++ server/api/response.js
@@ -3,17 +3,37 @@ const router = express.Router();
 const axios = require("axios");
 const HOST = "http://localhost:3001/registerUser";
 
+var knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "honey",
+  },
+});
+
 router.get("/login/success", async (req, res, next) => {
   res.statusCode = 200;
   if (req.headers.cookie !== undefined) {
     res.setHeader("Cookie", req.headers.cookie);
   }
   console.log(res.getHeaders());
+
+  knex
+    .select()
+    .from("user")
+    .then((result) => {
+      res.send(result[0]);
+    });
+  /*
   res.send({
     name: req.query.name,
     years: req.query.years,
     email: req.query.email,
   });
+  */
 });
 
 router.get("/login/error", async (req, res, next) => {
