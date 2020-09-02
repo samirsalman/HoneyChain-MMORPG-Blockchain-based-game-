d23ce6554e6a1398f5b630803bdaba3ed902e6fd
Samir <samirsalman1997@gmail.com>
2020-09-02 12:08:18 +0200

    Update invoke.js

--- fabrics-samples/fabcar/javascript/invoke.js
+++ fabrics-samples/fabcar/javascript/invoke.js
@@ -9,14 +9,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
-const cors = require("cors");
 //enables cors
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function () {
