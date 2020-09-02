809e892b2d97d8f26d990b6ed7e68e7fb36d0b55
Samir <samirsalman1997@gmail.com>
2020-09-02 12:06:51 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,14 +4,7 @@ const http = require("http");
 //Let's include the module to create quicky our  server
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
 //define the port on which the server will listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
