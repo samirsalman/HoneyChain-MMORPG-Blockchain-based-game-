3963f316ab320ac0bc0ec51902598ee35aff118b
Samir <samirsalman1997@gmail.com>
2020-09-03 17:20:27 +0200

    Update response.js

--- server/api/response.js
+++ server/api/response.js
@@ -111,8 +111,8 @@ router.get("/login/error", (req, res, next) => {
     res.setHeader("Cookie", req.headers.cookie);
   }
   console.log(res.getHeaders());
-  res.status(503).send({
-    error: req.query.error.replace("_", " "),
+  res.status(404).send({
+    error: "Utente non trovato",
   });
 });
 
