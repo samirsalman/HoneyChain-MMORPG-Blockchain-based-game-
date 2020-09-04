5dc0da32bcb80ca69e5fab729ee1010f787cb5bc
Samir <samirsalman1997@gmail.com>
2020-09-04 11:52:38 +0200

    getHistory Implemented

--- fabrics-samples/chaincode/fabcar/javascript/lib/fabhoney.js
+++ fabrics-samples/chaincode/fabcar/javascript/lib/fabhoney.js
@@ -1,11 +1,10 @@
-const { Contract } = require('fabric-contract-api');
+const { Contract } = require("fabric-contract-api");
 
 class FabHoney extends Contract {
-
-    async initLedger(ctx) {
-        console.info('============= START : Initialize Ledger ===========');
-        const gameObjects = [
-            {
+  async initLedger(ctx) {
+    console.info("============= START : Initialize Ledger ===========");
+    const gameObjects = [
+      {
         color: "purple",
         owner: "admin",
         power: 200,
@@ -54,74 +53,98 @@ class FabHoney extends Contract {
         level: 3,
         id: 6,
       },
-        ];
+    ];
 
-        for (let i = 0; i < gameObjects.length; i++) {
-            gameObjects[i].docType = 'honey';
-            await ctx.stub.putState('HONEY' + i, Buffer.from(JSON.stringify(gameObjects[i])));
-            console.info('Added <--> ', gameObjects[i]);
-        }
-        console.info('============= END : Initialize Ledger ===========');
+    for (let i = 0; i < gameObjects.length; i++) {
+      gameObjects[i].docType = "honey";
+      await ctx.stub.putState(
+        "HONEY" + i,
+        Buffer.from(JSON.stringify(gameObjects[i]))
+      );
+      console.info("Added <--> ", gameObjects[i]);
     }
+    console.info("============= END : Initialize Ledger ===========");
+  }
 
-    async queryHoney(ctx, honeyNumber) {
-        const honeyAsBytes = await ctx.stub.getState(honeyNumber); // get the car from chaincode state
-        if (!honeyAsBytes || honeyAsBytes.length === 0) {
-            throw new Error(`${honeyNumber} does not exist`);
-        }
-        console.log(honeyAsBytes.toString());
-        return honeyAsBytes.toString();
+  async queryHoney(ctx, honeyNumber) {
+    const honeyAsBytes = await ctx.stub.getState(honeyNumber); // get the car from chaincode state
+    if (!honeyAsBytes || honeyAsBytes.length === 0) {
+      throw new Error(`${honeyNumber} does not exist`);
     }
+    console.log(honeyAsBytes.toString());
+    return honeyAsBytes.toString();
+  }
 
-    async createHoney(ctx, honeyNumber, color, owner, power, level, id) {
-        console.info('============= START : Create Honey ===========');
+  async createHoney(ctx, honeyNumber, color, owner, power, level, id) {
+    console.info("============= START : Create Honey ===========");
 
-        const honey = {
-            color,
-            docType: 'honey',
-            owner,
-            power,
-            level,
-	    id,
-        };
+    const honey = {
+      color,
+      docType: "honey",
+      owner,
+      power,
+      level,
+      id,
+    };
 
-        await ctx.stub.putState(honeyNumber, Buffer.from(JSON.stringify(honey)));
-        console.info('============= END : Create Honey ===========');
+    await ctx.stub.putState(honeyNumber, Buffer.from(JSON.stringify(honey)));
+    console.info("============= END : Create Honey ===========");
+  }
+
+  async queryHoneys(ctx) {
+    const startKey = "HONEY0";
+    const endKey = "HONEY999";
+    const allResults = [];
+    for await (const { key, value } of ctx.stub.getStateByRange(
+      startKey,
+      endKey
+    )) {
+      const strValue = Buffer.from(value).toString("utf8");
+      let record;
+      try {
+        record = JSON.parse(strValue);
+      } catch (err) {
+        console.log(err);
+        record = strValue;
+      }
+      allResults.push({ Key: key, Record: record });
     }
+    console.info(allResults);
+    return JSON.stringify(allResults);
+  }
 
-    async queryHoneys(ctx) {
-        const startKey = 'HONEY0';
-        const endKey = 'HONEY999';
-        const allResults = [];
-        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
-            const strValue = Buffer.from(value).toString('utf8');
-            let record;
-            try {
-                record = JSON.parse(strValue);
-            } catch (err) {
-                console.log(err);
-                record = strValue;
-            }
-            allResults.push({ Key: key, Record: record });
-        }
-        console.info(allResults);
-        return JSON.stringify(allResults);
+  async changeHoneyOwner(ctx, honeyNumber, newOwner) {
+    console.info("============= START : changeOwner ===========");
+
+    const honeyAsBytes = await ctx.stub.getState(honeyNumber); // get the car from chaincode state
+    if (!honeyAsBytes || honeyAsBytes.length === 0) {
+      throw new Error(`${honeyNumber} does not exist`);
     }
+    const honey = JSON.parse(honeyAsBytes.toString());
+    honey.owner = newOwner;
 
-    async changeHoneyOwner(ctx, honeyNumber, newOwner) {
-        console.info('============= START : changeOwner ===========');
+    await ctx.stub.putState(honeyNumber, Buffer.from(JSON.stringify(honey)));
+    console.info("============= END : changeHoneyOwner ===========");
+  }
 
-        const honeyAsBytes = await ctx.stub.getState(honeyNumber); // get the car from chaincode state
-        if (!honeyAsBytes || honeyAsBytes.length === 0) {
-            throw new Error(`${honeyNumber} does not exist`);
-        }
-        const honey = JSON.parse(honeyAsBytes.toString());
-        honey.owner = newOwner;
+  async getHistory(ctx, key) {
+    const promiseOfIterator = ctx.stub.getHistoryForKey(key);
 
-        await ctx.stub.putState(honeyNumber, Buffer.from(JSON.stringify(honey)));
-        console.info('============= END : changeHoneyOwner ===========');
+    const results = [];
+    for await (const keyMod of promiseOfIterator) {
+      const resp = {
+        timestamp: keyMod.timestamp,
+        txid: keyMod.tx_id,
+      };
+      if (keyMod.is_delete) {
+        resp.data = "KEY DELETED";
+      } else {
+        resp.data = keyMod.value.toString("utf8");
+      }
+      results.push(resp);
     }
-
+    return results;
+  }
 }
 
 module.exports = FabHoney;
--- fabrics-samples/fabcar/javascript/query.js
+++ fabrics-samples/fabcar/javascript/query.js
@@ -9,6 +9,7 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const router = express.Router();
+const { Contract } = require("fabric-contract-api");
 
 async function main(username) {
   try {
@@ -134,6 +135,65 @@ async function changeOwner(email, id) {
   }
 }
 
+async function getHistory(id) {
+  try {
+    // load the network configuration
+    const ccpPath = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "test-network",
+      "organizations",
+      "peerOrganizations",
+      "org1.example.com",
+      "connection-org1.json"
+    );
+    let ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(email);
+    if (!identity) {
+      console.log(
+        'An identity for the user "appUser" does not exist in the wallet'
+      );
+      console.log("Run the registerUser.js application before retrying");
+      return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: email,
+      discovery: { enabled: true, asLocalhost: true },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork("mychannel");
+
+    // Get the contract from the network.
+    const contract = network.getContract("fabhoney");
+
+    // Submit the specified transaction.
+    // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+    //changeHoneyOwner transaction - requires 2 args , ex: ('changeHoneyOwner', 'HONEY0', 'Marcello')
+    var history = await contract.submitTransaction("getHistory", id);
+    console.log("History");
+
+    // Disconnect from the gateway.
+    await gateway.disconnect();
+    return history;
+  } catch (error) {
+    console.error(`Failed to get History: ${error}`);
+    process.exit(1);
+  }
+}
+
 router.get("/transaction", (req, res, next) => {
   changeOwner(req.query.email, req.query.id)
     .then(() => {
@@ -141,5 +201,32 @@ router.get("/transaction", (req, res, next) => {
     })
     .catch((err) => res.send(err));
 });
+
+router.get("/history", (req, res, next) => {
+  getHistory(req.query.id)
+    .then((res) => {
+      res.send(res);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+async function getHistory(key) {
+  const promiseOfIterator = ctx.stub.getHistoryForKey(key);
+
+  const results = [];
+  for await (const keyMod of promiseOfIterator) {
+    const resp = {
+      timestamp: keyMod.timestamp,
+      txid: keyMod.tx_id,
+    };
+    if (keyMod.is_delete) {
+      resp.data = "KEY DELETED";
+    } else {
+      resp.data = keyMod.value.toString("utf8");
+    }
+    results.push(resp);
+  }
+  return results;
+}
 
 module.exports = router;
