e36936fd8cd53ec371209203139214f9295d68a1
Samir <samirsalman1997@gmail.com>
2020-09-06 16:55:06 +0200

    Update query.js

--- fabrics-samples/fabcar/javascript/query.js
+++ fabrics-samples/fabcar/javascript/query.js
@@ -204,9 +204,7 @@ router.get("/transaction", (req, res, next) => {
 router.get("/history", (req, res, next) => {
   getHistory(req.query.id, req.query.email)
     .then((response) => {
-      response = response.map(
-        (el) => (el.data = JSON.parse(el.data.buffer.data.toString()))
-      );
+      response = response.map((el) => (el.data = JSON.parse(el.data)));
       response.data = JSON.parse(response.data.data.toString());
       res.send(JSON.parse(response));
     })
