66b54ba1ced88b670bd8d22eecdfe22f638c8256
Simone Giorgioni <symmy@DESKTOP-1UNKVD3.(none)>
2020-09-06 19:10:38 +0200

    validation

--- val rule/sdc.scm
+++ val rule/sdc.scm
@@ -97,8 +97,8 @@
 
 (defun Manage::HASHPASS (actionl pbuf)
   (eis::GiveErrorHTTP401) =>
-  (Show "Calculating password hash...")
-  ;; Prelevo dalla GET i campi email e password
+  (Show "Calcolo l'hash della password")
+  ;;Prendo i valori dalla richiesta GET
   (define mail (mtfa-eis-get-value-current-query pbuf "email"))
   (define pass (mtfa-eis-get-value-current-query pbuf "password"))
   (define name (mtfa-eis-get-value-current-query pbuf "name"))
@@ -108,14 +108,13 @@
   (system (string-append "echo -n " pass " | sha256sum | cut -d ' ' -f 1 > " rand))
   (define hash (read-line (open-file rand "r")))
   (system (string-append "rm " rand))
-  ;; Effettuo la redirect verso la pagina "fittizia" della VA con query email password e hash della password
   (eis::Redirect (string-append "/user/hashregister?email=" mail "&password=" hash "&name=" name "&years=" years ))
   )
 
 (defun Manage::LOGIN (actionl pbuf)
   (eis::GiveErrorHTTP401) =>
-  (Show "Calculating password hash...")
-  ;; Prelevo dalla GET i campi email e password
+  (Show "Calcolo l'hash della password")
+   ;;Prendo i valori dalla richiesta GET
   (define mail (mtfa-eis-get-value-current-query pbuf "email"))
   (define pass (mtfa-eis-get-value-current-query pbuf "password"))
   (define rand (number->string (mtfa-strong-random 256) 16))
@@ -123,23 +122,53 @@
   (system (string-append "echo -n " pass " | sha256sum | cut -d ' ' -f 1 > " rand))
   (define hash (read-line (open-file rand "r")))
   (system (string-append "rm " rand))
-  ;; Effettuo la redirect verso la pagina "fittizia" della VA con query email password e hash della password
-  (eis::Redirect (string-append "/controllo?email=" mail "&password=" hash ))
+  (eis::Redirect (string-append "/verificaDati?email=" mail "&password=" hash ))
   )
-
-(defun Manage::COOKIE_LOG (actionl pbuf)
+  
+  (defun Manage::LOGIN_GAME (actionl pbuf)
   (eis::GiveErrorHTTP401) =>
-  (Show "Setting login cookie...")
-  ;; Prelevo dalla GET i campi email, password e hash della password
+  (Show "Calculating password hash...")
+  ;;Prendo i valori dalla richiesta GET
   (define mail (mtfa-eis-get-value-current-query pbuf "email"))
   (define pass (mtfa-eis-get-value-current-query pbuf "password"))
-  (define hash (mtfa-eis-get-value-current-query pbuf "password"))
   (define rand (number->string (mtfa-strong-random 256) 16))
-  ;;  Controllo se l'hash della password corrisponde all'hash passato in query
+  ;;  Calcolo l'hash della password
   (system (string-append "echo -n " pass " | sha256sum | cut -d ' ' -f 1 > " rand))
-  (define hashPass (read-line (open-file rand "r")))
+  (define hash (read-line (open-file rand "r")))
   (system (string-append "rm " rand))
-  ;; Creo un cookie di login con il seguente formato RAND*EMAIL*HASH
+  (eis::Redirect (string-append "/controlloGame?email=" mail "&password=" hash ))
+  )
+
+(defun Manage::SETTO_COOKIE_LOGIN (actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (Show "Setto il cookie per il login")
+  ;;Prendo i valori dalla richiesta GET
+  (define mail (mtfa-eis-get-value-current-query pbuf "email"))
+  (define pass (mtfa-eis-get-value-current-query pbuf "password"))
+  (define rand (number->string (mtfa-strong-random 256) 16))
+  (system (string-append "echo -n " pass " | sha256sum | cut -d ' ' -f 1 > " rand))
+  ;; Creo un cookie per il login
+  (define x (number->string (mtfa-strong-random 5) 16))
+  (define cookie (string-append x "*" mail "*" pass))
+    ;; Codifico il cookie in base64
+  (define rand2 (number->string (mtfa-strong-random 256) 16))
+  (system (string-append "echo -n " cookie " | base64 -w 0 | sed s/=/@/g > " rand2))
+  (define cookieB64 (read-line (open-file rand2 "r")))
+  (system (string-append "rm " rand2))
+  ;;  Durata del cookie di 3 minuti
+  (define time (strftime "%c" (localtime (+ (time-second (current-time)) 180))))
+  (eis::Redirect (string-append "/verificaDati2?email=" mail "&password=" pass "&cookie=" cookieB64))
+  )
+
+(defun Manage::SETTO_COOKIE_LOGIN_GAME (actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (Show "Setto il cookie per il login nel gioco")
+  ;;Prendo i valori dalla richiesta GET
+  (define mail (mtfa-eis-get-value-current-query pbuf "email"))
+  (define pass (mtfa-eis-get-value-current-query pbuf "password"))
+  (define rand (number->string (mtfa-strong-random 256) 16))
+  (system (string-append "echo -n " pass " | sha256sum | cut -d ' ' -f 1 > " rand))
+  ;; Creo un cookie per il login nel gioco
   (define x (number->string (mtfa-strong-random 5) 16))
   (define cookie (string-append x "*" mail "*" hash))
     ;; Codifico il cookie in base64
@@ -147,28 +176,27 @@
   (system (string-append "echo -n " cookie " | base64 -w 0 | sed s/=/@/g > " rand2))
   (define cookieB64 (read-line (open-file rand2 "r")))
   (system (string-append "rm " rand2))
-  ;;  Imposto la validità del cookie a 30 minuti
+  ;;  Durata del cookie di 3 minuti
   (define time (strftime "%c" (localtime (+ (time-second (current-time)) 180))))
-  ;;(define redirect (string-append "Set-Cookie: login=" cookieB64 "; expires=" time))
- ;; (define redirect (string-append "Set-Cookie: login=" cookieB64 "; expires=" time))
-  ;; Se l'hash della password nella query corrisponde all'hash calcolato della password
-  ;;    effettuo una redirect su /controllo settando il cookie di login
-  ;;    altrimenti effettuo una redirect su /login.html
-  (eis::Redirect (string-append "/controllore?email=" mail "&password=" pass "&cookie=" cookieB64))
+  (define redirect (string-append "Set-Cookie: login=" cookieB64 "; expires=" time))
+  (eis::Redirect  "/controlloGame" redirect)
   )
-
   
   (defun Manage::SENDDATAREG (actionl pbuf)
   (eis::GiveErrorHTTP401) =>
-  ;; Prelevo dalla GET i campi email e password
+  ;;Prendo i valori dalla richiesta GET
   (define mail (mtfa-eis-get-value-current-query pbuf "email"))
-  ;; Effettuo la redirect verso la pagina "fittizia" della VA con query email password e hash della password
   (eis::Redirect (string-append "http://192.168.1.2:3000/response/registration/success?email=" mail))
   )
 
   (defun Manage::DATA (actionl pbuf)
   (eis::GiveErrorHTTP401) =>
   (eis::Redirect (string-append "http://192.168.1.2:3000/response/loginCookie/success"))
+  )
+  
+    (defun Manage::DATA_GAME (actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (eis::Redirect (string-append "http://192.168.1.2:3000/response/login/success"))
   )
   
     (defun Manage::REDIRECT_SERVER (actionl pbuf)
@@ -209,17 +237,42 @@
 	(define id (mtfa-eis-get-value-current-query pbuf "id"))
   (eis::Redirect (string-append "http://192.168.1.2:3001/query/transaction?id=" id "&email=" mail))
   )
+  
+       (defun Manage::LOG_GIUSTO(actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (Show "Login giusto")
+  (eis::Redirect (string-append "http://192.168.1.2:3000/response/loginGame/success"))
+  )
+  
+      (defun Manage::HISTORY(actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (Show "History")
+  (define mail (mtfa-eis-get-value-current-query pbuf "email"))
+  (define id (mtfa-eis-get-value-current-query pbuf "id"))
+  (eis::Redirect (string-append "http://192.168.1.2:3001/query/history?id=" id "&email=" mail))
+  )
 
+   (defun Manage::LOGIN_COOKIE_ERROR(actionl pbuf)
+  (eis::GiveErrorHTTP401) =>
+  (Show "Login giusto")
+  (eis::Redirect (string-append "http://192.168.1.2:3000/response/login/error"))
+  )
 
 ;;Add HOOK
 (eis::function-pointer-add "LOGIN" Manage::LOGIN)
+(eis::function-pointer-add "LOGIN_COOKIE_ERROR" Manage::LOGIN_COOKIE_ERROR)
+(eis::function-pointer-add "HISTORY" Manage::HISTORY)
+(eis::function-pointer-add "LOG_GIUSTO" Manage::LOG_GIUSTO)
+(eis::function-pointer-add "LOGIN_GAME" Manage::LOGIN_GAME)
 (eis::function-pointer-add "TRANSACTION" Manage::TRANSACTION)
 (eis::function-pointer-add "LOGIN_ERROR_MAIL" Manage::LOGIN_ERROR_MAIL)
 (eis::function-pointer-add "REGISTRATION_ERROR_MAIL" Manage::REGISTRATION_ERROR_MAIL)
 (eis::function-pointer-add "COOKIE_ERROR" Manage::COOKIE_ERROR)
 (eis::function-pointer-add "LOGIN_SUCCESS" Manage::LOGIN_SUCCESS)
-(eis::function-pointer-add "COOKIE_LOG" Manage::COOKIE_LOG)
+(eis::function-pointer-add "SETTO_COOKIE_LOGIN" Manage::SETTO_COOKIE_LOGIN)
+(eis::function-pointer-add "SETTO_COOKIE_LOGIN_GAME" Manage::SETTO_COOKIE_LOGIN_GAME)
 (eis::function-pointer-add "SENDDATAREG" Manage::SENDDATAREG)
 (eis::function-pointer-add "HASHPASS" Manage::HASHPASS)
 (eis::function-pointer-add "DATA" Manage::DATA)
+(eis::function-pointer-add "DATA_GAME" Manage::DATA_GAME)
 (eis::function-pointer-add "REDIRECT_SERVER" Manage::REDIRECT_SERVER)
